ebf469d5b09a59d2f9700cee875120070f14ecbb
Akim <44138907+akimrx@users.noreply.github.com>
2021-05-08 14:22:51 +0300

    Update plugins/modules/proxysql_query_rules_fast_routing.py
    Co-authored-by: Andrew Klychkov <aaklychkov@mail.ru>

--- plugins/modules/proxysql_query_rules_fast_routing.py
+++ plugins/modules/proxysql_query_rules_fast_routing.py
@@ -59,6 +59,8 @@ options:
         schedules deleted, you can set I(force_delete) to C(True).
     type: bool
     default: False
+notes:
+- Supports C(check_mode).
 extends_documentation_fragment:
 - community.proxysql.proxysql.managing_config
 - community.proxysql.proxysql.connectivity
